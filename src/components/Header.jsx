47b09b9248d6c821a9b6bb51647476c9b0c629b5
Saguneo <sagunmain@gmail.com>
2024-04-05 15:24:47 -0400

    header sizing fixes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,17 @@ import Axios from "axios";
 import { SearchBar } from "./Searchbar";
 import { Map } from "./Map";
 
-function DataDisplay({ title, value }) {
+function DataDisplay({ title, value, border }) {
   return (
-    <div>
-      <p className=" uppercase text-[10px] tracking-widest text-gray-400 font-bold">
+    <div
+      className={`${
+        border && `md:border-r-[${border}px]`
+      } md:border-gray-300 md:pr-10 md:pl-5 md:py-2`}
+    >
+      <p className=" uppercase text-[10px] md:text-xs mb-2 tracking-widest text-gray-400 font-bold">
         {title}
       </p>
-      <h1 className=" font-semibold text-lg">{value}</h1>
+      <h1 className=" font-semibold text-xl">{value}</h1>
     </div>
   );
 }
@@ -80,11 +84,11 @@ export default function Header() {
         <SearchBar userInput={userInput} fetchData={fetchData} />
       </div>{" "}
       {inError && <p className=" text-red-600 ">{inError}</p>}
-      <div className="flex flex-col gap-5 w-full max-w-[85%] mx-auto bottom-32 rounded-xl py-5 text-center bg-white relative z-[2] ca">
-        <DataDisplay title="ip address" value={ip} />
-        <DataDisplay title="location" value={location} />
-        <DataDisplay title="timezone" value={timezone} />
-        <DataDisplay title="isp" value={isp} />
+      <div className="flex flex-col md:flex-row md:items-center md:h-[150px] md:px-2 gap-5 md:gap-0 w-full max-w-[85%] md:max-w-[800px] mx-auto bottom-32 rounded-xl py-5 text-center md:text-start bg-white relative z-[2] ca">
+        <DataDisplay border="1" title="ip address" value={ip} />
+        <DataDisplay border="1" title="location" value={location} />
+        <DataDisplay border="1" title="timezone" value={timezone} />
+        <DataDisplay border="0" title="isp" value={isp} />
       </div>
       <div className=" absolute top-[120px] w-full z-[1]">
         <Map getLat={getLat} getLon={getLon} />
